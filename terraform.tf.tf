8c2485401e0741422e799f2088ba3e0ca2397d51
PruthviChavan21 <105622653+PruthviChavan21@users.noreply.github.com>
2022-07-21 17:29:13 +0530

    Add files via upload

---
+++ terraform.tf.tf
@@ -0,0 +1,26 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+
+# Resource: EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = "ami-0742b4e673072066f"
+  instance_type = "t3.micro"
+  user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
